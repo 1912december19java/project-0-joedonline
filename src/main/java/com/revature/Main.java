9420a0bf49f8b5c115300d7dac84516acb932274
Joe Domingo <contact@joedomingo.online>
2019-12-23 11:35:58 -0500

    PROJECT 0 - initial setup.

--- src/main/java/com/revature/Main.java
+++ src/main/java/com/revature/Main.java
@@ -6,7 +6,33 @@ package com.revature;
  * Try not to have any logic at all on this class.
  */
 public class Main {
+	
+	/*
+	 * USER STORIES:
+	 * 
+	 * [in progress...] "As a user I can login and logout."
+	 * [ ] "As a user I can view my balance."
+	 * [ ] "As a user I can withdraw money."
+	 * [ ] "As a user I can deposit money."
+	 * [ ] "As a user I can register."
+	 * [ ] (optional) "As a user I can view all my transactions."
+	 * [ ] (optional) "As a user I can transfer money from my account to another account."
+	 */
 
+	/*
+	 * "As a user I can login and logout."
+	 * 
+	 * { State :: fields }
+	 * - username
+	 * - password
+	 * - sanitized
+	 * 
+	 * { Behavior :: methods }
+	 * - loginRequest()
+	 * - isLoggedIn()
+	 * - logoutRequest()
+	 */
+	
 	public static void main(String[] args) {
 		System.out.println("Testing STS3, project 0.");
 	}
---
+++ src/test/resources/canLoginAndLogout.java
@@ -0,0 +1,35 @@
+/*
+ * "As a user I can login and logout."
+ * 
+ * { State :: fields }
+ * - username
+ * - password
+ * - sanitized
+ * 
+ * { Behavior :: methods }
+ * - loginRequest()
+ * - isLoggedIn()
+ * - logoutRequest()
+ */
+
+public class canLoginAndLogout {
+
+	// test fields here...
+	String username;
+	String password;
+	String sanitized;
+	
+	// test methods here...
+	void loginRequest() {
+		
+	}
+	
+	boolean isLoggedIn() {
+		
+	}
+	
+	void logoutRequest() {
+		
+	}
+	
+}
